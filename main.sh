c3e865c67b5062b1fa4eb3ef03636d68f534194b
chenenkou <chenenkou@163.com>
2023-07-20 17:28:07 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+# 设置目标时区为东八区（中国标准时间）
+export TZ="Asia/Shanghai"
+
+if [ -f .env ]; then
+  source .env
+fi
+
 key="${PUSH_KEY}"
 
-current_time=$(date +"%Y年%m月%d日 %H:%M:%S")  
+current_time=$(date +"%Y年%m月%d日 %H:%M:%S")
 
 text="现在时间 $current_time"
 text="${text// /%20}"
+echo $text
 
 url="https://api2.pushdeer.com/message/push?pushkey=${key}&text=${text}"
-
 echo $url
-
 curl -X GET "$url"
